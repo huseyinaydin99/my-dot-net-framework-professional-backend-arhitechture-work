a49b29b064d0829354eff467e82fcf8f6c2a5b8f
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-09-24 18:27:33 +0300

    NHibernate için ProductMap eşlemesi yapıldı.

---
+++ AydinCompany.Northwind.DataAccess/Concrete/NHibernate/Mappings/ProductMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AydinCompany.Northwind.Entities.ComplexType;
+using FluentNHibernate.Mapping;
+
+namespace AydinCompany.Northwind.DataAccess.Concrete.NHibernate.Mappings
+{
+    public class ProductMap : ClassMap<Product>
+    {
+        public ProductMap()
+        {
+            Table(@"Products");
+            LazyLoad();
+
+            Id(x => x.ProductId).Column("ProductID");
+
+            Map(x => x.CategoryId).Column("CategoryID");
+            Map(x => x.ProductName).Column("ProductName");
+            Map(x => x.QuantityPerUnit).Column("QuantityPerUnit");
+            Map(x => x.UnitPrice).Column("UnitPrice");
+        }
+    }
+}
