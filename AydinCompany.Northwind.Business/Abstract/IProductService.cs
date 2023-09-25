560647b82d446513a8e051630039943cbb8c0869
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-09-25 12:01:05 +0300

    İş katmanı(business layer) implementasyonu yapıldı.

---
+++ AydinCompany.Northwind.Business/Abstract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using AydinCompany.Northwind.Entities.ComplexType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AydinCompany.Northwind.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        Product GetById(int id);
+        Product Add(Product product);
+    }
+}
---
+++ AydinCompany.Northwind.Business/Concrete/Manager/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AydinCompany.Northwind.Business.Abstract;
+using AydinCompany.Northwind.DataAccess.Abstract;
+using AydinCompany.Northwind.Entities.ComplexType;
+
+namespace AydinCompany.Northwind.Business.Concrete.Manager
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productDal.GetList();
+        }
+
+        public Product GetById(int id)
+        {
+            return _productDal.Get(p => p.ProductId == id);
+        }
+
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+    }
+}
