2d4639a9126fef88a985524873b35f411d24fdd5
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-09-27 13:39:36 +0300

    MemoryCache implementasyonu yapıldı. Bir sonraki admında MemoryCache'nin Aspect'i yazılacak.

---
+++ AydinCompany.Core/CrossCuttingConserns/Caching/Microsoft/MemoryCacheManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using NHibernate.Util;
+
+namespace AydinCompany.Core.CrossCuttingConserns.Caching.Microsoft
+{
+    public class MemoryCacheManager : ICacheManager
+    {
+        protected ObjectCache Cache => MemoryCache.Default;
+        public T Get<T>(string key)
+        {
+            return (T)Cache[key];
+        }
+
+        public void Add(string key, object data, int cacheTime)
+        {
+            if (data == null)
+            {
+                return;
+            }
+
+            var policy = new CacheItemPolicy
+            {
+                AbsoluteExpiration = DateTime.Now + TimeSpan.FromMinutes(cacheTime)
+            };
+            Cache.Add(key, data, policy);
+        }
+
+        public bool IsAdd(string key)
+        {
+            return Cache.Contains(key);
+        }
+
+        public void Remove(string key)
+        {
+            Cache.Remove(key);
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            var regex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var keysToRemove = Cache.Where(d => regex.IsMatch(d.Key)).Select(d => d.Key).ToList();
+            keysToRemove.ForEach(key =>
+            {
+                Remove(key);
+            });
+        }
+
+        public void Clear()
+        {
+            Cache.ForEach(item =>
+            {
+                Remove(item.Key);
+            });
+        }
+    }
+}
