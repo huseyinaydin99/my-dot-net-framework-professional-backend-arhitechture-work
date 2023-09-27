555503e85c83c75d7d6493b1b5cf3f055a794b1e
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-09-27 16:08:29 +0300

    Loglama için Log4Net implementasyonu yapıldı. Bir sonraki adım Aspect yazılmasıdır.

---
+++ AydinCompany.Core/CrossCuttingConserns/Logging/LogDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AydinCompany.Core.CrossCuttingConserns.Logging
+{
+    //Loglanacak metohod'a ait property'ler.
+    //O method'a ait 
+    public class LogDetail
+    {
+        public string FullName { get; set; }
+        public string MethodName { get; set; }
+        public List<LogParameter> Parameters { get; set; }
+    }
+}
---
+++ AydinCompany.Core/CrossCuttingConserns/Logging/LogParameter.cs
@@ -0,0 +1,10 @@
+﻿namespace AydinCompany.Core.CrossCuttingConserns.Logging
+{
+    //Log data'sını tutacak bir nesne.
+    public class LogParameter
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public object Value { get; set; }
+    }
+}
