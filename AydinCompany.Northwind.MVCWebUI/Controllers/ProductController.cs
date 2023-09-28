45b970d2db98322b03cd48a2d935335246e13a34
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-09-28 12:14:28 +0300

    MVC tarafı için ProductController sınıfı oluşturuldu. Index metoduna bir View bağlandı.

---
+++ AydinCompany.Northwind.MVCWebUI/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AydinCompany.Northwind.Business.Abstract;
+
+namespace AydinCompany.Northwind.MVCWebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+
+        public ProductController()
+        {
+            
+        }
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var model = new ProductListViewModel()
+            {
+                Products = _productService.GetAll()
+            };
+            return View(model);
+        }
+    }
+}
---
+++ AydinCompany.Northwind.MVCWebUI/Models/ProductListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AydinCompany.Northwind.Entities.ComplexType;
+using System.Collections.Generic;
+
+namespace AydinCompany.Northwind.MVCWebUI
+{
+    public class ProductListViewModel
+    {
+        public List<Product> Products { get; set; }
+    }
+}
