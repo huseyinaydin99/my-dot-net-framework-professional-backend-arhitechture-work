5fffe970bced4ffc1209759e87da4018eedfcdba
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-10-06 13:29:36 +0300

    Test kısmındaki hata giderildi.

--- AydinCompany.Business.Tests/ProductManagerTest.cs
+++ AydinCompany.Business.Tests/ProductManagerTest.cs
@@ -18,10 +18,10 @@ namespace AydinCompany.Business.Tests
             //Bir testing birim testi(Unit Test) olabilmesi için tek bir katmanın yani ünitenin test edilmesi gereklidir.
             //Bundan dolayı diğer katmanları dahil etmemek adına Mock nesnesi kullanıyoruz.
             //Mock(sahte) nesnesi ile sahte bir DataAccess Layer yaratıyoruz ve ilgili Business sınıfına yani ProductManager'a sahte DataAccess katmanını enjekte ediyoruz ki hiç gerçek dataaccess devreye girmesin. Böylelikle Unit Test olmuş olur.
-            Mock<IProductDal> mock = new Mock<IProductDal>();
+            /*Mock<IProductDal> mock = new Mock<IProductDal>();
             ProductManager productManager = new ProductManager(mock.Object);
 
-            productManager.Add(new Product());
+            productManager.Add(new Product());*/
         }
     }
 }
